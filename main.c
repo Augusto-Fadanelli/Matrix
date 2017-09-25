345dbc3e97ead511a11d073a331ba1cc2096153e
Augusto Fadanelli de Sousa <32231958+Augusto-Fadanelli@users.noreply.github.com>
2017-09-24 22:01:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void matrixvalues()
+//Matrix type and its values
+int mValues()
 {
 
     //Declaration of variables
-    int i, j; // i: Line; j: Column;
+    int i, j; // i: Line counter; j: Column counter;
     int lines, columns;
 
     //Defining the Matrix type
@@ -17,11 +18,31 @@ void matrixvalues()
     int matrix [lines] [columns]; //Matrix vector declaration
 
     //Showing the type of Matrix
-    printf("Matriz do tipo: %dx%d \n", lines, columns);
-    for(i = 1; i <= lines; i++){
-        for(j = 1; j <= columns; j++){
+    printf("Matriz do tipo: %dx%d ", lines, columns);
+    if(lines == columns){
 
-            printf("a%d%d ", i, j);
+        printf("(Matriz quadrada).");
+
+    }
+
+    else if(lines == 1 && columns > 1){
+
+        printf("(Matriz linha).");
+
+    }
+
+    else if(lines > 1 && columns == 1){
+
+        printf("(Matriz coluna).");
+
+    }
+
+    printf("\n");
+
+    for(i = 0; i < lines; i++){
+        for(j = 0; j < columns; j++){
+
+            printf("a%d%d ", i + 1, j + 1);
 
         }
         printf("\n");
@@ -50,41 +71,131 @@ void matrixvalues()
             printf("\n");
     }
 
+    return 0;
+
 }
 
 void main()
 {
 
     //Declaration of variables
-    int op, end; //op: option
+    static int op;
+    int end; //op: option
 
     printf("\n***CALCULOS DE MATRIZES*** \n \n");
 
     do{
 
-        printf("Digite 1 para calculos com uma Matriz apenas \nou 2 para calculos com duas Matrizes \n");
+        printf("Digite:\n1 - Calculos com uma Matriz apenas\n2 - Calculos com duas Matrizes\n");
         scanf("%d", &op);
 
         switch(op){
 
             case 1:
-            printf("teste 1 \n");
+
+                printf("Digite:\n1 - Matriz Transposta\n2 - Matriz Oposta\n3 - Matriz inversa\n");
+                printf("4 - Multiplicar um numero real por uma Matriz\n5 - Matriz Determinante\n");
+                printf("6 - Cofator de uma Matriz\n7 - Teorema de Laplace\n");
+                scanf("%d", &op);
+
+                switch(op){
+
+                    case 1:
+
+                        mTransposed();
+
+                    break;
+
+                    case 2:
+
+                        mValues();
+
+                    break;
+
+                    case 3:
+
+                        mValues();
+
+                    break;
+
+                    case 4:
+
+                        mValues();
+
+                    break;
+
+                    case 5:
+
+                        mValues();
+
+                    break;
+
+                    case 6:
+
+                        mValues();
+
+                    break;
+
+                    case 7:
+
+                        mValues();
+
+                    break;
+
+                    default:
+                        printf("Opçao invalida.\n");
+
+                }
+
             break;
 
-            matrixvalues();
-
             case 2:
-            printf("teste 2 \n");
+
+                printf("Digite:\n1 - Adiçao de Matrizes\n2 - Subtraçao de Matrizes\n3 - Multiplicaçao de Matrizes\n");
+                scanf("%d", &op);
+
+                switch(op){
+
+                    case 1:
+
+                        mValues();
+
+                    break;
+
+                    case 2:
+
+                        mValues();
+
+                    break;
+
+                    case 3:
+
+                        mValues();
+
+                    break;
+
+                    default:
+                        printf("Opçao invalida.\n");
+
+                }
+
             break;
 
             default:
-            printf("Opçao invalida.\n");
+                printf("Opçao invalida.\n");
 
         }
 
-        printf("Digite 1 para continuar ou digite qualquer outro para sair: \n");
+        printf("\nDigite 1 para continuar ou digite qualquer outro para sair: \n");
         scanf("%d", &end);
 
     }while (end == 1);
+
+}
+
+void mTransposed()
+{
+
+    mValues();
 
 }
